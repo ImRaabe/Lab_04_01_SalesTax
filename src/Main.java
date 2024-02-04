dcb10f70b87d7236fb6e419948835a650c51ec1f
ImRaabe <raabees@mail.uc.edu>
2024-02-04 02:22:25 -0500

    Lab04.1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-
-        }
+        double purchasePrice = 100;
+        double salesTax = purchasePrice * 0.05;
+        System.out.println("The sales tax for your purchase is: $" + salesTax + " and the total cost of your purchase is: $" + salesTax + purchasePrice);
     }
+}
